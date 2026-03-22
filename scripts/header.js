590db29915b84edd43a0473bf45dff51fa428004
eyosiyas12345 <eyosiyasgezahegn326@gmail.com>
2026-03-23 01:25:45 +0300

    bar menu added

--- scripts/header.js
+++ scripts/header.js
@@ -1,18 +1,16 @@
 
 //Add a header to all sections.
 fetch('/components/header.html').then(response =>{
- if(!response.ok){
-  throw new Error ("Header file is not found");
- }
-  return response.text()
+ if(!response.ok)  throw new Error ("Header file is not found");
+  return response.text();
 } ).then(headerString => {
-    document.getElementById("header").innerHTML = headerString;
+    const headerContainer = document.getElementById("header");
+    headerContainer.innerHTML = headerString;
 
-    // active page link accessing 
-    let parser = new DOMParser();
-    let headerDom = parser.parseFromString(headerString, 'text/html');
-    const navLinks = headerDom.querySelectorAll('.nav-link');
+    // active page link accessing & showing current page
+    const navLinks = headerContainer.querySelectorAll('.nav-link');
     const currentPathName = window.location.pathname.split('/').pop();
+    if(!currentPathName) currentPathName = 'index.html';
     console.log("succeed");
     console.log(navLinks);
     console.log(currentPathName);
@@ -24,6 +22,15 @@ fetch('/components/header.html').then(response =>{
             link.classList.add('active');
             console.log("succeed");
         }
-    })
-
+    });
+    // humburger
+    const humberger = document.getElementById("humberger");
+    const navLinksContainer = document.querySelector(".nav-links");
+    
+    if(humberger && navLinksContainer){
+        humberger.addEventListener("click",()=>{
+            humberger.classList.toggle('active');
+            navLinksContainer.classList.toggle('active');
+        });
+    }
 }).catch(error => console.log('The Error:', error));
